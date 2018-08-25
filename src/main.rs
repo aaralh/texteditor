73a67a5ae37a89a953e2775f7cd3bcea4052fa2f
Aaro Alhainen <aaro.alhainen@gmail.com>
2018-08-25 09:50:24 +0300

    Added initial support for reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn read_file(fileuri: String) {
+    println!("In file {}", fileuri);
+
+    let mut f = File::open(fileuri).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
+
+fn write_file()
+
 fn main() {
     println!("Hello, world!");
+    let fileuri = "./text.txt";
+    read_file(fileuri.to_string());
 }
