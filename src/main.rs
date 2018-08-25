e3fa1386de74c3094aa46af8ddf22db56d7c08db
Aaro Alhainen <aaro.alhainen@gmail.com>
2018-08-25 17:17:45 +0300

    Improved code structure by moving editor features to own function. Also added comments

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ fn read_file(fileuri: String) -> String {
 
     let path = Path::new(&fileuri);
 
+    // Try open file. If no file found return empty string.
     let mut f = match File::open(path) {
         Err(_why) => return String::new(),
         Ok(f) => f,
@@ -47,11 +48,44 @@ fn save_file(fileuri: String, content: String) -> bool {
     }
 }
 
+// Editor mode handler.
+fn editor_mode(content: String) {
+
+    // Init raw mode.
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    let stdin = stdin();
+
+    write!(stdout,
+            "{}{}",
+            cursor::Goto(1, 1),
+            content).unwrap();
+    // Flush contents to screen.
+    stdout.flush().unwrap();
+
+    for c in stdin.keys() {
+        // Print the key we type...
+        match c.unwrap() {
+            Key::Char('q') => break,
+            Key::Char('\n') => print!("\n\r"),
+            Key::Char(c)   => print!("{}", c),
+            Key::Alt(c)    => println!("Alt-{}", c),
+            Key::Ctrl(c)   => println!("Ctrl-{}", c),
+            Key::Left      => println!("<left>"),
+            Key::Right     => println!("<right>"),
+            Key::Up        => println!("<up>"),
+            Key::Down      => println!("<down>"),
+            _              => println!("Other"),
+        }
+        // Flush again.
+        stdout.flush().unwrap();
+    }
+    // Move cursor to begin of next line.
+    write!(stdout, "\n\r").unwrap();
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
-
-    let stdin = stdin();
 
     // Clearing screen
     println!("{}", clear::All);
@@ -60,34 +94,9 @@ fn main() {
     if args.len() == 2 {
         let fileuri = env::args().nth(1).expect("Missing argument");;
         let content = read_file(fileuri);
-
-        let mut stdout = stdout().into_raw_mode().unwrap();
-
-        write!(stdout,
-                "{}{}", 
-                cursor::Goto(1, 1),
-                content).unwrap();
-
-        stdout.flush().unwrap();
-
-        for c in stdin.keys() {
-            // Print the key we type...
-            match c.unwrap() {
-                Key::Char('q') => break,
-                Key::Char('\n') => print!("\n\r"),
-                Key::Char(c)   => print!("{}", c),
-                Key::Alt(c)    => println!("Alt-{}", c),
-                Key::Ctrl(c)   => println!("Ctrl-{}", c),
-                Key::Left      => println!("<left>"),
-                Key::Right     => println!("<right>"),
-                Key::Up        => println!("<up>"),
-                Key::Down      => println!("<down>"),
-                _              => println!("Other"),
-            }
-
-            // Flush again.
-            stdout.flush().unwrap();
-        }
+        editor_mode(content);
+    } else {
+        editor_mode("".to_string())
     }
 }
 
