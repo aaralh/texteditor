7070c674a9e8212ab1f1e41df0bc4cc68b8b8edf
Aaro Alhainen <aaro.alhainen@gmail.com>
2018-08-25 12:43:57 +0300

    Added initial functionality to write to file and test for it

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::fs::File;
 use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
 
 fn read_file(fileuri: String) -> String {
     let mut f = File::open(fileuri).expect("file not found");
@@ -11,17 +12,42 @@ fn read_file(fileuri: String) -> String {
     return contents;
 }
 
+fn save_file(fileuri: String, content: String) -> bool {
+
+    let path = Path::new(&fileuri);
+
+    let mut file = match File::create(&path) {
+        Err(_why) => return false,
+        Ok(file) => file,
+    };
+
+    match file.write_all(content.as_bytes()) {
+        Err(_why) => {
+            return false;
+        },
+        Ok(_) => return true,
+    }
+}
+
 fn main() {
 }
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
+
+    #[test]
+    fn test_save_file() {
+        let fileuri = "./src/text.txt";
+        let content = "This is test text.".to_string();
+        assert_eq!(save_file(fileuri.to_string(), content), true);
+    }
 
     #[test]
     fn test_read_file() {
         let fileuri = "./src/text.txt";
-        assert_eq!(read_file(fileuri.to_string()), "This is text.".to_string());
+        let content = "This is test text.".to_string();
+        assert_eq!(read_file(fileuri.to_string()), content);
     }
+
 }
