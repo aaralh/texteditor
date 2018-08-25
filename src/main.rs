d2e52d7d9d50ea12661361c76facee0c62d1232c
Aaro Alhainen <aaro.alhainen@gmail.com>
2018-08-25 12:03:32 +0300

    Added test for read file

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn read_file(fileuri: String) {
-    println!("In file {}", fileuri);
-
+fn read_file(fileuri: String) -> String {
     let mut f = File::open(fileuri).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    return contents;
 }
 
-fn write_file()
-
 fn main() {
-    println!("Hello, world!");
-    let fileuri = "./text.txt";
-    read_file(fileuri.to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_read_file() {
+        let fileuri = "./src/text.txt";
+        assert_eq!(read_file(fileuri.to_string()), "This is text.".to_string());
+    }
 }
